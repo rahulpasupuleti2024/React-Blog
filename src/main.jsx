284c82f952ceb5ca396b41da5c8a204bc7ca1c80
rahulpasupuleti2024 <rahulpasupuleti2@gmail.com>
2023-10-04 12:17:38 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home.jsx'
+import Home from './Pages/Home.jsx'
 import { AuthLayout, Login } from './components/index.js'
 
 
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
     children: [
         {
             path: "/",
-            element: <Home />,
+            element: <Home/>,
         },
         {
             path: "/login",
